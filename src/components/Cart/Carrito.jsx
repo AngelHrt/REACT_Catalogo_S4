869914b5381c479bad14296b876df8e2cab7f2f0
AngelHrt <saiber798@gmail.com>
2024-07-06 15:50:49 -0500

    carrito operativo

--- src/components/Cart/Carrito.jsx
+++ src/components/Cart/Carrito.jsx
@@ -1,19 +1,23 @@
+
+
 const Carrito = ({cart}) => {
+    const total = cart.reduce((acc, item) => acc + item.price * item.quantity, 0);
     return (
         <div id="carrito" className="col-4 bg-secondary mt-2 mb-2">
 			<h3>Carrito de Compras</h3>
 			<ul id="cont-cart">
                 {cart.map((item,index) =>(
                     <li key={index}>
-                        {item.name} - Cantidad: {item.quantity}
+                        {item.name} - Precio: {item.price}- Cantidad: {item.quantity}
                     </li>
                 )
                 )}
 			</ul>
-			<p>TOTAL: <span id="total-cart">S/ 0</span></p>
+			<p>TOTAL: <span id="total-cart">S/ {total}</span></p>
 			<button class="btn btn-primary">PAGAR</button>
 		</div>
     );
 };
 
 export default Carrito;
+
